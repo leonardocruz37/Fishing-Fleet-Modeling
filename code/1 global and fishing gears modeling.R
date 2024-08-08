03122a68a98ecb9d588f2b05e1cfbada29a6cfd2
Leonardo Cruz <57488767+leonardocruz37@users.noreply.github.com>
2024-08-08 13:50:16 +0200

    Add files via upload

---
+++ code/1 global and fishing gears modeling.R
@@ -0,0 +1,131 @@
+library(embarcadero)
+library(fuzzySim)
+
+##  CLEAR EVERYTHING
+rm(list = ls(all.names = TRUE))
+
+set.seed(2024)
+
+path <- "~/Project Fishing Fleet Modeling"
+
+# Raster data
+setwd(paste0(path, "/environmental variables/present/"))
+var_stack <- stack(list.files(".", full.names = TRUE, pattern = '.nc'))
+var_stack <- raster::aggregate(var_stack, 1/res(var_stack))
+
+setwd(paste0(path, "/environmental variables/ssp4_2100/"))
+var_stack_f85 <- stack(list.files(".", full.names = TRUE, pattern = '.nc'))
+var_stack_f85 <- raster::aggregate(var_stack_f85, 1/res(var_stack_f85))
+
+setwd(paste0(path, "/environmental variables/ssp1_2100/"))
+var_stack_f26 <- stack(list.files(".", full.names = TRUE, pattern = '.nc'))
+var_stack_f26 <- raster::aggregate(var_stack_f26, 1/res(var_stack_f26))
+setwd(path)
+
+# MMSI data
+dat.all <- read.csv(paste0(path,'/data/mmsi_2013-2020_processed.csv')) %>%
+  left_join(read.csv(paste0(path,'/data/metadata.csv'))[,c(1,4,8)], by = 'mmsi') %>%
+  filter(vessel_class_gfw != 'fishing') %>%
+  na.omit()
+
+vessel_classes <- sort(unique(dat.all$vessel_class_gfw))
+list <- c('Global',vessel_classes)
+
+varimp_df <- list()
+partial_df <- list()
+for(do in list){
+  if(do == 'Global'){
+    ifelse(!dir.exists(file.path(paste0(path,'/output/'), do)), dir.create(file.path(paste0(path,'/output/'), do)), FALSE)
+    do_path <- paste0(path,'/output/', do)
+    
+    dat.presence <- dat.all %>%
+      select(cell_ll_lon,cell_ll_lat) %>%
+      setNames(., c('x','y')) %>%
+      distinct()
+    
+    dat <- fuzzySim::gridRecords(var_stack, dat.presence)
+    dat <- dat[complete.cases(dat),]
+  }
+  
+  if(do %in% vessel_classes){
+    ifelse(!dir.exists(file.path(paste0(path,'/output/fishing gears/'), do)), dir.create(file.path(paste0(path,'/output/fishing gears/'), do)), FALSE)
+    do_path <- paste0(path,'/output/fishing gears/', do)
+    
+    dat.presence <- dat.all %>%
+      filter(vessel_class_gfw == do) %>%
+      select(cell_ll_lon,cell_ll_lat) %>%
+      setNames(., c('x','y')) %>%
+      distinct()
+    
+    dat <- fuzzySim::gridRecords(var_stack, dat.presence)
+    dat <- dat[complete.cases(dat),]
+  }
+  
+  #Input data
+  names(dat)
+  spc_col <- 1 # presence (1) - absence (0) column
+  var_cols <- 5:12 # predictor variable columns
+  names(dat)[spc_col]
+  names(dat)[var_cols]
+  myspecies <- names(dat)[spc_col]
+  myspecies
+  
+  #Modeling
+  mod_BART <- bart(x.train = dat[ , var_cols], y.train = dat[ , myspecies], keeptrees = TRUE)
+  
+  # Save summary
+  sink(paste0(do_path, '/Table 1. BART summary (', do,').txt'), type = 'output')
+  print(summary(mod_BART))
+  sink()
+  
+  #Save the diagnostic plot
+  ggsave(paste0(path, '/diagnostic plots/',do,'.png'), width = 7,height = 5)
+  
+  # Variable importance
+  varimp_plot <- varimp(mod_BART, plots = TRUE)
+  varimp_plot$class <- do
+  varimp_df <- rbind(varimp_df, varimp_plot)
+  
+  # Raster predictions
+  BART_P <- as.data.frame(predict2.bart(mod_BART, var_stack, quantiles=c(0.025, 0.975)),xy=TRUE)
+  BART_F85 <- as.data.frame(predict2.bart(mod_BART, var_stack_f85, quantiles=c(0.025, 0.975)),xy=TRUE)
+  BART_F26 <- as.data.frame(predict2.bart(mod_BART, var_stack_f26, quantiles=c(0.025, 0.975)),xy=TRUE)
+  
+  # 'layer' is the posterior mean
+  colnames(BART_P) <- c('x','y','layer','lower_95_ci','upper_95_ci')
+  colnames(BART_F85) <- c('x','y','layer','lower_95_ci','upper_95_ci')
+  colnames(BART_F26) <- c('x','y','layer','lower_95_ci','upper_95_ci')
+  
+  # Difference
+  BART_DIFF85 <- data.frame(x=BART_P[,1],
+                            y=BART_P[,2],
+                            layer = BART_F85[,3] - BART_P[,3],
+                            lower_95_ci = BART_F85[,4] - BART_P[,4],
+                            upper_95_ci = BART_F85[,5] - BART_P[,5])
+  
+  BART_DIFF26 <- data.frame(x=BART_P[,1],
+                            y=BART_P[,2],
+                            layer=BART_F26[,3] - BART_P[,3],
+                            lower_95_ci = BART_F26[,4] - BART_P[,4],
+                            upper_95_ci = BART_F26[,5] - BART_P[,5])
+  
+  # Save predictions
+  write.csv(BART_P, paste0(do_path,'/BART_P (',do,').csv'),row.names=FALSE)
+  write.csv(BART_F85, paste0(do_path,'/BART_F85 (',do,').csv'),row.names=FALSE)
+  write.csv(BART_F26, paste0(do_path,'/BART_F26 (',do,').csv'),row.names=FALSE)
+  
+  write.csv(BART_DIFF85, paste0(do_path,'/BART_DIFF85 (',do,').csv'),row.names=FALSE)
+  write.csv(BART_DIFF26, paste0(do_path,'/BART_DIFF26 (',do,').csv'),row.names=FALSE)
+  
+  # Presence points based on model threshold
+  threshold <- summary(mod_BART)[[3]][["plot_env"]][["thresh"]]
+  write.csv(BART_P %>% filter(layer >= threshold), paste0(do_path,'/BART_P_presence (',do,').csv'),row.names=FALSE)
+  write.csv(BART_F85 %>% filter(layer >= threshold), paste0(do_path,'/BART_F85_presence (',do,').csv'),row.names=FALSE)
+  write.csv(BART_F26 %>% filter(layer >= threshold), paste0(do_path,'/BART_F26_presence (',do,').csv'),row.names=FALSE)
+  
+  # Delete model to free memory space
+  rm(mod_BART)
+  
+}
+
+write.csv(varimp_df, paste0(path,'/output/variable_importance_gear_global.csv'), row.names = FALSE)
---
+++ code/4 global richness and beta calculation.R
@@ -0,0 +1,73 @@
+library(dplyr)
+library(betapart)
+
+##  CLEAR EVERYTHING
+rm(list = ls(all.names = TRUE))
+
+path <- "~/Project Fishing Fleet Modeling"
+
+country_list <- unlist(read.table(paste0(path,'/data/countries.txt'), header = FALSE))
+
+#Getting all the coordinates of the world map
+df_p <- read.csv(paste0(path,'/output/countries/ARG/BART_P (ARG).csv'))[,1:2]
+df_85 <- read.csv(paste0(path,'/output/countries/ARG/BART_P (ARG).csv'))[,1:2]
+df_26 <- read.csv(paste0(path,'/output/countries/ARG/BART_P (ARG).csv'))[,1:2]
+
+# Creating the presence-absence 'species community matrix', where each row (cell) is a community
+# and the columns species (countries).
+for(do in country_list){
+  
+  df_p <- df_p %>%
+    left_join(read.csv(paste0(path,'/output/countries/',do,'/BART_P_presence (',do,').csv')) %>%
+                select(x, y, layer) %>%
+                mutate(layer = 1) %>%
+                setNames(.,c('x','y', do)), by = c('x','y'))
+  
+  df_85 <- df_85 %>%
+    left_join(read.csv(paste0(path,'/output/countries/',do,'/BART_F85_presence (',do,').csv')) %>%
+                select(x, y, layer) %>%
+                mutate(layer = 1) %>%
+                setNames(.,c('x','y', do)), by = c('x','y'))
+  
+  df_26 <- df_26 %>%
+    left_join(read.csv(paste0(path,'/output/countries/',do,'/BART_F26_presence (',do,').csv')) %>%
+                select(x, y, layer) %>%
+                mutate(layer = 1) %>%
+                setNames(.,c('x','y', do)), by = c('x','y'))
+  
+}
+
+df_p[is.na(df_p)] <- 0
+df_85[is.na(df_85)] <- 0
+df_26[is.na(df_26)] <- 0
+
+# SSP4
+
+# Richness
+df85 <- cbind(df_p[,1:2], rowSums(df_85[,3:84]) - rowSums(df_p[,3:84]),rowSums(df_85[,3:84]), rowSums(df_p[,3:84])) %>%
+  setNames(., c('x','y','rich_difference','rich85','rich_p'))
+
+# Beta diversity
+beta.temp_85 <- list()
+for(i in 1:64800){
+  beta.temp_85 <- rbind(beta.temp_85, beta.temp(df_p[i,3:84],df_85[i,3:84], index.family = 'sorensen'))
+}
+
+df85 <- cbind(df85, beta.temp_85)
+write.csv(df85, paste0(path, '/output/richness_beta_SSP4.csv'), row.names = FALSE)
+
+
+# SSP1
+
+# Richness
+df26 <- cbind(df_p[,1:2], rowSums(df_26[,3:84]) - rowSums(df_p[,3:84]),rowSums(df_26[,3:84]), rowSums(df_p[,3:84])) %>%
+  setNames(., c('x','y','rich_difference','rich26','rich_p'))
+
+# Beta diversity
+beta.temp_26 <- list()
+for(i in 1:64800){
+  beta.temp_26 <- rbind(beta.temp_26, beta.temp(df_p[i,3:84],df_26[i,3:84], index.family = 'sorensen'))
+}
+
+df26 <- cbind(df26, beta.temp_26)
+write.csv(df26, paste0(path,'/output/richness_beta_SSP1.csv'), row.names = FALSE)
